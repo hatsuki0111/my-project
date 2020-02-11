884fcf2d2cbeb8447addf612dafe156b7cf818e3
hatsuki0111 <cist.b217.h.saitou@gmail.com>
2020-02-12 00:25:17 +0900

    githbpages設定

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
- git push -f git@github.com:hatsuki0111/hatsuki0111.github.io.git master
+#git push -f git@github.com:hatsuki0111/hatsuki0111.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:hatsuki0111/my-project.git master:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production',
-  outputDir: 'docs'
-  ? '/hatsuki0111.github.io/'
+  publicPath: process.env.NODE_ENV === 'production'
+  //outputDir: 'docs'
+  ? '/my-project/'
   : '/'
 }

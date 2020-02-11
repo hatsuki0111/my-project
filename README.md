30f7948eceacfbba010a22b8a00af7ae4d092be8
hatsuki0111 <cist.b217.h.saitou@gmail.com>
2020-02-11 19:53:30 +0900

    outputDir: 'docs'

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# portfoio
+# portfolio
 
 > A Vue.js project
 
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
+  publicPath: process.env.NODE_ENV === 'production',
+  outputDir: 'docs'
   ? '/hatsuki0111.github.io/'
   : '/'
 }

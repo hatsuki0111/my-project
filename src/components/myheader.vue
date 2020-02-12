3ff780634275ba1c96abc70618bbd97ca57d5401
hatsuki0111 <cist.b217.h.saitou@gmail.com>
2020-02-12 20:25:19 +0900

    Navbarのレスポンシブ化

--- src/components/myheader.vue
+++ src/components/myheader.vue
@@ -1,23 +1,25 @@
 <template>
     <div>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <router-link class="text-white px-3" to="/">home</router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link class="text-white px-3" to="/test1">test1</router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link class="text-white px-3" to="/test2">test2</router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link class="text-white px-3" to="/test3">test3</router-link>
-                </li>
-                </ul>
-            </div>
-        </nav>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item>
+           <router-link class="text-white px-3" to="/">home</router-link>
+        </b-nav-item>
+        <b-nav-item>
+           <router-link class="text-white px-3" to="/test1">test1</router-link>
+        </b-nav-item>
+        <b-nav-item>
+           <router-link class="text-white px-3" to="/test2">test2</router-link>
+        </b-nav-item>
+        <b-nav-item>
+           <router-link class="text-white px-3" to="/test3">test3</router-link>
+        </b-nav-item>
+      </b-navbar-nav>
+      </b-collapse>
+  </b-navbar>
     </div>
 </template>
 
